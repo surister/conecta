72519ba4d381125b02e7ca76921c675b6f8d3c16
surister <surister98@gmail.com>
2025-05-25 18:00:48 +0200

    Add loggers module with methods to log memory usage + arrow + tokio_postgres dependencies

---
+++ conecta-core/src/logger.rs
@@ -0,0 +1,13 @@
+use peak_alloc::PeakAlloc;
+
+
+#[global_allocator]
+static PEAK_ALLOC: PeakAlloc = PeakAlloc;
+
+pub fn log_memory(){
+    log::debug!("[DEBUG] Current memory usage: {}MB", PEAK_ALLOC.current_usage_as_mb())
+}
+
+pub fn log_peak_memory(){
+    log::debug!("[DEBUG] Peak memory usage: {}MB", PEAK_ALLOC.peak_usage_as_mb())
+}

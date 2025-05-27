dbb71e5f3aa03633f9dbd7c18708a52e70ebab33
surister <surister98@gmail.com>
2025-05-27 11:14:23 +0200

    Experiment: Try sending different query partitions in different threads
    Note: Commits marked with `Experiment` are commits --typically modifying main.rs-- that do not really contain changes to the libraries themselves, but rather, changes that I use to investigate and understand things, these commits are useful if you want to understand how things evolve from my perspective. Otherwise, these commits can be safely ignored.

--- conecta-core/src/main.rs
+++ conecta-core/src/main.rs
@@ -1,74 +1,60 @@
-mod logger;
-
-use std::sync::Arc;
-use arrow::array::{Int32Builder};
-use arrow::datatypes::{DataType, Field, Schema};
-use arrow::record_batch::RecordBatch;
-use tokio_postgres::{Error, NoTls};
-
-use crate::logger::{log_memory_with_label, log_peak_memory};
-
-static URI: &str = "postgres://postgres:postgres@192.168.88.251:5400/postgres";
-static QUERY: &str = "select * from lineitem";
+use tokio_postgres::{NoTls, Error};
+use tokio::task;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     use std::time::Instant;
     let now = Instant::now();
+    // Set up first connection
+    let (client1, connection1) =
+        tokio_postgres::connect("postgres://postgres:postgres@192.168.88.251:5400/postgres", NoTls).await?;
+    tokio::spawn(async move {
+        if let Err(e) = connection1.await {
+            eprintln!("connection1 error: {}", e);
+        }
+    });
 
-    env_logger::init();
+    // Set up second connection
+    let (client2, connection2) =
+        tokio_postgres::connect("postgres://postgres:postgres@192.168.88.251:5400/postgres", NoTls).await?;
+    tokio::spawn(async move {
+        if let Err(e) = connection2.await {
+            eprintln!("connection2 error: {}", e);
+        }
+    });
 
-    log_memory_with_label("Pre-allocation of Arrow builders");
+    let (client3, connection2) =
+        tokio_postgres::connect("postgres://postgres:postgres@192.168.88.251:5400/postgres", NoTls).await?;
+    tokio::spawn(async move {
+        if let Err(e) = connection2.await {
+            eprintln!("connection2 error: {}", e);
+        }
+    });
 
-    // Pre allocate memory with pre-known counts.
-    let mut builder = Int32Builder::with_capacity(1199969);
-    let mut builder2 = Int32Builder::with_capacity(1199969);
+    // Run both queries in parallel tasks
+    let query1 = task::spawn(async move {
+        let rows = client1.query("select * from lineitem where l_orderkey >= 0 and l_orderkey < 399989", &[]).await?;
+        println!("Query 1 result: {:?}", rows.len());
+        Ok::<_, Error>(())
+    });
 
-    log_memory_with_label("Post allocation of Arrow builders");
+    let query2 = task::spawn(async move {
+        let rows = client2.query("select * from lineitem where l_orderkey >= 399989 and l_orderkey < 799978", &[]).await?;
+        println!("Query 2 result: {:?}", rows.len());
+        Ok::<_, Error>(())
+    });
 
-    let (client, connection) = tokio_postgres::connect(URI, NoTls).await?;
-    
-    tokio::spawn(connection);
+    let query3 = task::spawn(async move {
+        let rows = client3.query("select * from lineitem where l_orderkey > 799978", &[]).await?;
+        println!("Query 3 result: {:?}", rows.len());
+        Ok::<_, Error>(())
+    });
 
-    let rows = client.query(QUERY, &[]).await?;
-    
-    log_memory_with_label("Data is fetched");
-    
-    let mut buff1 = Vec::with_capacity(1199969);
-    let mut buff2 = Vec::with_capacity(1199969);
-    
-    for row in rows {
-        let value: i32 = row.get(0);
-        let value2: i32 = row.get(1);
-        buff1.push(value);
-        buff2.push(value2);
-    }
-    
-    log_memory_with_label("Data is deserialized into buffers");
-    
-    builder.append_slice(&buff1);
-    builder2.append_slice(&buff2);
+    // Wait for both queries to finish
+    query1.await.unwrap()?;
 
-    let arrow_array = builder.finish();
-    let arrow_array2 = builder2.finish();
-
-    let schema = Schema::new(vec![
-        Field::new("your_column_name", DataType::Int32, false),
-        Field::new("col2", DataType::Int32, false)
-    ]);
-
-    let record_batch = RecordBatch::try_new(
-        Arc::new(schema),
-        vec![Arc::new(arrow_array), Arc::new(arrow_array2)],
-    ).unwrap();
-    
-    println!("{:?}", record_batch.num_rows());
-    
-    log_memory_with_label("Record batch is created and freed");
-    
     let elapsed = now.elapsed();
-    
+
     println!("Elapsed: {:.2?}", elapsed);
-    log_peak_memory();
     Ok(())
 }

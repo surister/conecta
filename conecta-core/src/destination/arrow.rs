ed6f125dfd366794aa009d7053fc797fccf57f8d
surister <surister98@gmail.com>
2025-07-18 14:54:36 +0200

    Implement arrow builders for several NativeTypes

--- conecta-core/src/destination/arrow.rs
+++ conecta-core/src/destination/arrow.rs
@@ -1,34 +1,59 @@
-use arrow::array::{ArrayBuilder, BooleanBuilder, Float32Builder, Float64Builder, Int16Builder, Int32Builder, Int64Builder, Int8Builder, PrimitiveArray, PrimitiveBuilder, StringBuilder, UInt16Builder, UInt32Builder, UInt64Builder, UInt8Builder};
-use arrow::datatypes::{Float64Type, Int32Type};
 use crate::destination::destination::Destination;
 use crate::schema::{NativeType, Schema};
+use arrow::array::{ArrayBuilder, BooleanBuilder, Date32Builder, Float16Builder, Float32Builder, Float64Builder, Int16Builder, Int32Builder, Int64Builder, Int8Builder, PrimitiveBuilder, StringBuilder, UInt16Builder, UInt32Builder, UInt64Builder, UInt8Builder};
+use arrow::datatypes::{Float64Type, Int32Type};
 
 pub struct ArrowDestination {}
+macro_rules! get_builders {
+    ($schema:expr, { $($dtype:pat => $builder:ident),+ $(,)? }) => {{
+        let mut builders: Vec<Box<dyn ArrayBuilder>> = Vec::new();
+        for column in &$schema.columns {
+            match column.data_type {
+                $(
+                    $dtype => builders.push(Box::new($builder::new())),
+                )+
+                _ => unimplemented!("Arrow builder not implemented for {:?}", column.data_type),
+            }
+        }
+        builders
+    }};
 
-pub enum MyBuilder {
-    Bool(BooleanBuilder),
-    Int8(Int8Builder),
-    Int16(Int16Builder),
-    Int32(Int32Builder),
-    Int64(Int64Builder),
-    UInt8(UInt8Builder),
-    UInt16(UInt16Builder),
-    UInt32(UInt32Builder),
-    UInt64(UInt64Builder),
-    Float32(Float32Builder),
-    Float64(Float64Builder),
-    String(StringBuilder),
+    ($schema:expr, $capacity:expr, { $($dtype:pat => $builder:ident),+ $(,)? }) => {{
+        let mut builders: Vec<Box<dyn ArrayBuilder>> = Vec::new();
+        for column in &$schema.columns {
+            match column.data_type {
+                $(
+                    $dtype => builders.push(Box::new($builder::with_capacity($capacity))),
+                )+
+                _ => unimplemented!("Arrow builder not implemented for {:?}", column.data_type),
+            }
+        }
+        builders
+    }};
 }
 
-impl MyBuilder {
+pub fn get_arrow_builders(schema: Schema) -> Vec<Box<dyn ArrayBuilder>> {
+    get_builders!(schema, {
+        NativeType::Bool => BooleanBuilder,
 
-    pub fn append_nulls(&mut self, n: usize){
-        match self { 
-            MyBuilder::Int32(b) => b.append_nulls(n),
-            MyBuilder::Float64(b) => b.append_nulls(n),
-            _ => todo!()
-        }
-    }
+        NativeType::I8 => Int8Builder,
+        NativeType::I16 => Int16Builder,
+        NativeType::I32 => Int32Builder,
+        NativeType::I64 => Int64Builder,
+
+        NativeType::UI8 => UInt8Builder,
+        NativeType::UI16 => UInt16Builder,
+        NativeType::UI32 => UInt32Builder,
+        NativeType::UI64 => UInt64Builder,
+
+        NativeType::F16 => Float16Builder,
+        NativeType::F32 => Float32Builder,
+        NativeType::F64 => Float64Builder,
+
+        NativeType::String => StringBuilder,
+
+        NativeType::Date => Date32Builder
+    })
 }
 
 impl Destination for ArrowDestination {
@@ -44,31 +69,4 @@ impl Destination for ArrowDestination {
     fn allocate_schema(&self, schema: Schema) -> Vec<Box<dyn ArrayBuilder>> {
         todo!()
     }
-    
-    fn make_builders(&self, types: Vec<NativeType>, n: usize) -> Vec<MyBuilder> {
-    types
-        .into_iter()
-        .map(|t| match t {
-            NativeType::Bool => MyBuilder::Bool(BooleanBuilder::with_capacity(n)),
-            NativeType::Char | NativeType::String => {
-                MyBuilder::String(StringBuilder::with_capacity(n, n * 10)) // heuristic string size
-            }
-            NativeType::I8 => MyBuilder::Int8(Int8Builder::with_capacity(n)),
-            NativeType::I16 => MyBuilder::Int16(Int16Builder::with_capacity(n)),
-            NativeType::I32 => MyBuilder::Int32(Int32Builder::with_capacity(n)),
-            NativeType::I64 => MyBuilder::Int64(Int64Builder::with_capacity(n)),
-            NativeType::U8 => MyBuilder::UInt8(UInt8Builder::with_capacity(n)),
-            NativeType::U16 => MyBuilder::UInt16(UInt16Builder::with_capacity(n)),
-            NativeType::U32 => MyBuilder::UInt32(UInt32Builder::with_capacity(n)),
-            NativeType::U64 => MyBuilder::UInt64(UInt64Builder::with_capacity(n)),
-            NativeType::F32 => MyBuilder::Float32(Float32Builder::with_capacity(n)),
-            NativeType::F64 => MyBuilder::Float64(Float64Builder::with_capacity(n)),
-
-            NativeType::I128 | NativeType::U128 => {
-                panic!("128-bit integers are not supported by Apache Arrow")
-            }
-        })
-        .collect()
-}
-
 }

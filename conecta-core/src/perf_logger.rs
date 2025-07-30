f131c854cbdd7ba4428299d209408a23dd69305f
surister <surister98@gmail.com>
2025-07-30 13:45:19 +0200

    Remove '_' in struct members.
     Privacy in structs are the default in rust. This is a python idiom.

--- conecta-core/src/perf_logger.rs
+++ conecta-core/src/perf_logger.rs
@@ -8,27 +8,27 @@ use std::time::{Duration, Instant};
 
 pub struct PerfLogger {
     checkpoint_count: usize,
-    _start: Option<Instant>,
-    _last: Option<Duration>,
+    start: Option<Instant>,
+    last: Option<Duration>,
 }
 
 impl PerfLogger {
     pub fn start(&mut self) {
-        self._start = Some(Instant::now());
+        self.start = Some(Instant::now());
     }
     pub fn new() -> Self {
         PerfLogger {
             checkpoint_count: 0,
-            _start: None,
-            _last: Some(Duration::new(0, 0)),
+            start: None,
+            last: Some(Duration::new(0, 0)),
         }
     }
 
     pub fn new_started() -> Self {
         let mut metadata = PerfLogger {
             checkpoint_count: 0,
-            _start: None,
-            _last: Some(Duration::new(0, 0)),
+            start: None,
+            last: Some(Duration::new(0, 0)),
         };
         metadata.start();
         metadata
@@ -44,15 +44,15 @@ impl PerfLogger {
             "Checkpoint[{new_checkpoint}]: {message}, since Checkpoint[{last_checkpoint}]: {delta:?} | {memory}",
             new_checkpoint=self.checkpoint_count + 1,
             last_checkpoint=self.checkpoint_count,
-            delta=self._start.unwrap().elapsed() - self._last.unwrap(),
+            delta=self.start.unwrap().elapsed() - self.last.unwrap(),
             memory=memory
         );
-        self._last = Some(self._start.unwrap().elapsed());
+        self.last = Some(self.start.unwrap().elapsed());
         self.checkpoint_count += 1;
     }
 
     pub fn log_elapsed(&self) {
-        match self._start {
+        match self.start {
             None => panic!("Cannot print total duration without calling .start() first"),
             Some(instant) => log::debug!("\nTotal elapsed: {:.2?}", instant.elapsed()),
         }
@@ -62,7 +62,7 @@ impl PerfLogger {
         log::debug!("Peak memory usage: {}MB", PEAK_ALLOC.peak_usage_as_mb())
     }
     pub fn elapsed(&self) -> Duration {
-        self._start.unwrap().elapsed()
+        self.start.unwrap().elapsed()
     }
 }
 

9798b25c389e19edc63dd3b85441bf745acc6c0f
surister <surister98@gmail.com>
2025-07-31 18:44:02 +0200

    Make StringBuilder configurable

--- conecta-core/src/destination/arrow.rs
+++ conecta-core/src/destination/arrow.rs
@@ -1,10 +1,14 @@
-use crate::schema::Schema;
-use arrow::array::{make_builder, ArrayBuilder};
+use crate::schema::{NativeType, Schema};
+use arrow::array::{make_builder, ArrayBuilder, StringBuilder};
 
 pub fn get_arrow_builders(schema: &Schema, capacity: usize) -> Vec<Box<dyn ArrayBuilder>> {
     let mut builders: Vec<Box<dyn ArrayBuilder>> = Vec::with_capacity(schema.columns.len());
     for column in schema.columns.iter() {
-        builders.push(make_builder(&column.data_type.to_arrow(), capacity))
+        if column.data_type == NativeType::String {
+            builders.push(Box::new(StringBuilder::with_capacity(capacity, 1024)))
+        } else {
+            builders.push(make_builder(&column.data_type.to_arrow(), capacity))
+        }
     }
     builders
 }

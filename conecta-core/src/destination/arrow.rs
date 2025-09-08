f85f3239d42947aebacaf527914dacae80f6c721
surister <surister98@gmail.com>
2025-09-08 21:10:47 +0200

    Add support for `UUID`, `UUID_ARRAY`, `INT2_ARRAY`, `INT8_ARRAY`, `FLOAT4_ARRAY` and `FLOAT8_ARRAY`

--- conecta-core/src/destination/arrow.rs
+++ conecta-core/src/destination/arrow.rs
@@ -1,7 +1,7 @@
 use crate::schema::{NativeType, Schema};
 use arrow::array::{
-    make_builder, ArrayBuilder, Float32Builder, Float64Builder, Int16Builder, Int32Builder,
-    Int64Builder, ListBuilder, StringBuilder,
+    make_builder, ArrayBuilder, FixedSizeBinaryBuilder, Float32Builder, Float64Builder,
+    Int16Builder, Int32Builder, Int64Builder, ListBuilder, StringBuilder,
 };
 
 pub fn get_arrow_builders(schema: &Schema, capacity: usize) -> Vec<Box<dyn ArrayBuilder>> {
@@ -16,7 +16,9 @@ pub fn get_arrow_builders(schema: &Schema, capacity: usize) -> Vec<Box<dyn Array
             NativeType::VecI64 => builders.push(Box::new(ListBuilder::new(Int64Builder::new()))),
             NativeType::VecF32 => builders.push(Box::new(ListBuilder::new(Float32Builder::new()))),
             NativeType::VecF64 => builders.push(Box::new(ListBuilder::new(Float64Builder::new()))),
-
+            NativeType::VecUUID => {
+                builders.push(Box::new(ListBuilder::new(FixedSizeBinaryBuilder::new(16))))
+            }
             NativeType::BidimensionalPoint => builders.push(Box::new(ListBuilder::with_capacity(
                 Float64Builder::new(),
                 2,

cb87250a278c0d363e9ba13972ea7fcca04ade08
surister <surister98@gmail.com>
2025-10-09 15:58:06 +0200

    update docs

--- conecta-core/src/destination/arrow.rs
+++ conecta-core/src/destination/arrow.rs
@@ -8,13 +8,17 @@ pub fn get_arrow_builders(schema: &Schema, capacity: usize) -> Vec<Box<dyn Array
     let mut builders: Vec<Box<dyn ArrayBuilder>> = Vec::with_capacity(schema.columns.len());
     for column in schema.columns.iter() {
         match column.data_type {
-            NativeType::String => builders.push(Box::new(StringBuilder::with_capacity(capacity, 1024))),
+            NativeType::String => {
+                builders.push(Box::new(StringBuilder::with_capacity(capacity, 1024)))
+            }
             NativeType::VecI16 => builders.push(Box::new(ListBuilder::new(Int16Builder::new()))),
             NativeType::VecI32 => builders.push(Box::new(ListBuilder::new(Int32Builder::new()))),
             NativeType::VecI64 => builders.push(Box::new(ListBuilder::new(Int64Builder::new()))),
             NativeType::VecF32 => builders.push(Box::new(ListBuilder::new(Float32Builder::new()))),
             NativeType::VecF64 => builders.push(Box::new(ListBuilder::new(Float64Builder::new()))),
-            NativeType::VecString => builders.push(Box::new(ListBuilder::new(StringBuilder::new()))),
+            NativeType::VecString => {
+                builders.push(Box::new(ListBuilder::new(StringBuilder::new())))
+            }
             NativeType::VecUUID => {
                 builders.push(Box::new(ListBuilder::new(FixedSizeBinaryBuilder::new(16))))
             }
